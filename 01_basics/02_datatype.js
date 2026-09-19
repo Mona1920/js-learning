82fbd1494e4dea6d2fc15e54012a1f2267a385f7
MONALISHA BISWAS <bmonalisha1920@gmail.com>
2026-09-19 17:26:14 +0000

    datatypes

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -1,8 +1,27 @@
 "use strict"; // treat all JS code as newer version
 
-// alert("hello") //we're using nodejs, not browser
+// alert("3 + 3") //we're using nodejs, not browser
 
 console.log(3
     +3) //code readability should be high 
 
 console.log("Mona")
+
+let name = "Mona"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
